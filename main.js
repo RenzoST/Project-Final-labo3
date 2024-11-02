31a7250b2bf5d0ab3a4ddd17e2945180bdd2d06b
RenzoST <renzosotelo10@gmail.com>
2024-11-02 19:30:28 -0300

    crea seccion de juegos, boton de mostrar mas y la barra de busqueda

--- main.js
+++ main.js
@@ -1,1 +1,70 @@
-console.log("Hello World");
+const allGamesEl = document.querySelector('#allgames');
+const resultsEl = document.querySelector('#results');
+const showMoreBtnEl = document.querySelector('#btn-show-more');
+const searchEl = document.querySelector('#search');
+const formEl = document.querySelector('form');
+
+let page = 1;
+
+const npcs = [];
+const weapons = [];
+
+const { VITE_API_URL: apiUrl, VITE_ACCESS_KEY: accessKey } = import.meta.env;
+
+
+fetchNpcs()
+
+async function fetchNpcs() 
+{
+
+  let inputData = searchEl.value;
+
+  let url = new URL(apiUrl);
+  url.searchParams.append("search", inputData);
+  url.searchParams.append("key", accessKey);
+  url.searchParams.append("page", page);
+
+  if (page === 1) {
+    resultsEl.innerHTML = "";
+  }
+
+  const response = await fetch(url);
+  const { results } = await response.json();
+
+  results?.map((result) => {
+    const imageWrapper = document.createElement("article");
+    imageWrapper.classList.add("search-result");
+
+    const image = document.createElement("img");
+    image.src = result.background_image;
+
+    const name = document.createElement("h3");
+    name.textContent = result.name;
+
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(name);
+
+    resultsEl.appendChild(imageWrapper);
+  });
+
+  if( results.length < 20 ){
+    showMoreBtnEl.style.display = "none";
+  }
+  else{
+    showMoreBtnEl.style.display = "block";
+  }
+  
+  console.log(results);
+  page++;
+
+}
+
+showMoreBtnEl.addEventListener("click", () => fetchNpcs());
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  fetchNpcs();
+});
+
