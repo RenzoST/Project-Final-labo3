6e34a9c74c9b5b92b570418c22a5b99fbe11ed8f
Renzo <renzosotelo10@gmail.com>
2024-11-08 06:10:45 -0300

    base update

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: 'https://RenzoST.github.io/Project-Final-labo3', // Reemplaza <USERNAME> con tu nombre de usuario de GitHub
+  build: {
+    outDir: 'dist', // Esto es donde Vite guardará los archivos de construcción
+  }
+})
